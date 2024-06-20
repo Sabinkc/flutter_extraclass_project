7fb70178f723ef62cab58c8b9d4f8980462aeeb2
Sabinkc <sabinkc1206@gmail.com>
2024-06-20 15:19:43 +0545

    project page completed

--- lib/project.dart
+++ lib/project.dart
@@ -14,36 +14,56 @@ class Project extends StatelessWidget {
         title: Text("Project"),
         centerTitle: true,
       ),
-      body: Stack(
-        clipBehavior: Clip.none,
+      body: Column(
         children: [
-          Container(
-            height: 300,
-            color: Colors.red,
-          ),
-          Container(
-            height: 200,
-            margin: EdgeInsets.all(30),
-            decoration: BoxDecoration(
-                color: Colors.grey,
-                image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(
-                        "https://media.istockphoto.com/id/498240659/photo/two-nepali-women-spinning-a-wool-in-bhaktapur-nepal.jpg?s=1024x1024&w=is&k=20&c=E0sIB_gHJVJc2Tq1OWTm7dO3JeUmwfYwvgpdTYLskDI="))),
+          Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                height: 300,
+                color: Colors.red,
+              ),
+              Container(
+                height: 200,
+                margin: EdgeInsets.all(30),
+                decoration: BoxDecoration(
+                    color: Colors.grey,
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            "https://media.istockphoto.com/id/498240659/photo/two-nepali-women-spinning-a-wool-in-bhaktapur-nepal.jpg?s=1024x1024&w=is&k=20&c=E0sIB_gHJVJc2Tq1OWTm7dO3JeUmwfYwvgpdTYLskDI="))),
+              ),
+              SizedBox(
+                height: 325,
+              ),
+              Positioned(
+                  bottom: 0,
+                  left: screenWidth / 2 - 25,
+                  child: FloatingActionButton(
+                    backgroundColor: Colors.yellow,
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(Icons.arrow_back),
+                  )),
+            ],
           ),
           SizedBox(
-            height: 325,
-          ),
-          Positioned(
-              bottom: 0,
-              left: screenWidth / 2 - 25,
-              child: FloatingActionButton(
-                backgroundColor: Colors.yellow,
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Icon(Icons.arrow_back),
-              )),
+            height: 50,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: List.generate(
+                  20,
+                  (index) => Card(
+                        child: Container(
+                          width: 50,
+                          height: 50,
+                          color: Colors.red,
+                          child: Text("List ${index + 1}"),
+                        ),
+                      )),
+            ),
+          )
         ],
       ),
     );
